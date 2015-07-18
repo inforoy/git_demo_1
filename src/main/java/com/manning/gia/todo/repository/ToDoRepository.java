c25306d510135b1feaae7baa0b6a2bd6cd4625c5
royw <rwcalles@gmail.com>
2015-07-18 16:58:41 -0500

    commit espacio en blanco

--- src/main/java/com/manning/gia/todo/repository/ToDoRepository.java
+++ src/main/java/com/manning/gia/todo/repository/ToDoRepository.java
@@ -5,6 +5,7 @@ import com.manning.gia.todo.model.ToDoItem;
 import java.util.List;
 
 public interface ToDoRepository {
+
     List<ToDoItem> findAll();
     List<ToDoItem> findAllActive();
     List<ToDoItem> findAllCompleted();
@@ -12,4 +13,5 @@ public interface ToDoRepository {
     Long insert(ToDoItem toDoItem);
     void update(ToDoItem toDoItem);
     void delete(ToDoItem toDoItem);
+    
 }
